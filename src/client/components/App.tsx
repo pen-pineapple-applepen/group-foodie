a2e5789dc8e6e8d44653304f5ee52936a6840ccb
Erik Oh <eoh555@gmail.com>
2021-07-13 16:36:47 -0700

    Create plus minus components, input field component

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { useAppDispatch, useAppSelector } from '../state/hooks';
-import { Button, Form } from 'react-bulma-components';
-import { BackArrow, ProfileImage, OrangeButton, OrangeNavbar } from '../styles/shared';
 import allActions from '../state/actions/allActions';
 import styled from 'styled-components';
 
@@ -14,7 +12,7 @@ function App() {
 // these are just some test examples
   return (
     <div className="App">
-      <OrangeNavbar needBackArrow={true}/>
+
     </div>
   );
 }
--- src/client/styles/shared.tsx
+++ src/client/styles/shared.tsx
@@ -12,7 +12,14 @@ interface orangeNavbarProps {
   needBackArrow?: boolean,
   onBackArrowClick?: () => void,
 }
-
+interface plusButtonProps {
+  onClick?: () => void,
+  size: number,
+}
+interface minusButtonProps {
+  onClick?: () => void,
+  size: number,
+}
 interface backArrowProps {
   onClick?: () => void,
 }
@@ -25,6 +32,8 @@ const OrangeNavbarContainer = styled(Navbar)`
   display: flex;
   justify-content: center;
   align-items: center;
+  height: 60px;
+  border-radius: 0 0 22px 22px;
 `
 const NavbarBrand = styled(Navbar.Brand)`
   display: flex;
@@ -41,17 +50,47 @@ const BackArrowContainer = styled(Icon)`
   padding-right: 65px;
 `
 const GroupFoodieLogo = styled.p`
+  &:hover {
+    cursor: pointer;
+  }
   color: white;
 `
+const PlusButtonContainer = styled(Icon)`
+  &:hover {
+    cursor: pointer;
+  }
+  color: #FF6C36;
+`
+const MinusButtonContainer = styled(Icon)`
+  &:hover {
+    cursor: pointer;
+  }
+  color: #FF6C36;
+`
 
-
-   /* exported components */
+/////////////////////////
+/* exported components */
+/////////////////////////
 
 export const OrangeButton = styled(Button)`
   background-color: #FF6C36;
   border-radius: 30px;
   color: white;
 `
+export const OrangeInput = styled.input`
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid #FF6C36;
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid #2e6bbb
+  }
+`
+
+/////////////////////////////////////
+/* exported complete JSX components*/
+/////////////////////////////////////
+
 // navbar takes 2 props 'needBackArrow' that takes a boolean for if it should have a back arrow or not
 // and 'onBackArrowclick' which takes a callback for when back arrow is clicked
 export const OrangeNavbar: (props: orangeNavbarProps) => JSX.Element = ({needBackArrow, onBackArrowClick}) => {
@@ -66,7 +105,7 @@ export const OrangeNavbar: (props: orangeNavbarProps) => JSX.Element = ({needBac
             Group Foodie
           </GroupFoodieLogo>
         </NavbarItem>
-      <NavbarBurger/>
+      <NavbarBurger className="is-size-2"/>
       </NavbarBrand>
     </OrangeNavbarContainer>
   )
@@ -82,11 +121,26 @@ export const ProfileImage: (props: profileImageProps) => JSX.Element = ({src, si
 // BackArrow takes 1 prop 'onClick' and behaves the same way that a normal JSX element's 'onClick' would behave
 export const BackArrow: (props: backArrowProps) => JSX.Element = ({onClick}) => {
   return (
-    // <BackArrowContainer>
-    //   <Image src="backArrow.png" alt="back-arrow" size={16} onClick={onClick}/>
-    // </BackArrowContainer>
     <BackArrowContainer>
       <i className="fas fa-angle-left has-text-white-ter is-size-4"/>
     </BackArrowContainer>
+  )
+}
+
+// prop 'size' takes any number from 1 to 6, 1 being the largest.
+export const PlusButton: (props: plusButtonProps) => JSX.Element = ({onClick, size}) => {
+  return (
+    <PlusButtonContainer onClick={onClick}>
+      <i className={`fas fa-plus-circle is-size-${size || 3}`}/>
+    </PlusButtonContainer>
+  )
+}
+
+// prop 'size' takes any number from 1 to 6, 1 being the largest.
+export const MinusButton: (props: minusButtonProps) => JSX.Element = ({onClick, size}) => {
+  return (
+    <MinusButtonContainer onClick={onClick}>
+      <i className={`fas fa-plus-circle is-size-${size || 3}`}/>
+    </MinusButtonContainer>
   )
 }
