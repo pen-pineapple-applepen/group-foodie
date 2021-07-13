fb576ca48c18c88d934e62fe9ea66f6bff7a3301
Kenneth Doan <ken@Kenneths-MacBook-Pro.local>
2021-07-13 16:50:43 -0700

    Centered button and greeting

--- src/client/components/LandingPage.jsx
+++ src/client/components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar, Columns } from 'react-bulma-components';
 import styled from "styled-components";
+import { BackArrow, ProfileImage, OrangeButton, OrangeNavbar } from '../styles/shared';
 
 const LandingPage = () => {
   const StyledButton = styled.button`
@@ -8,11 +9,13 @@ const LandingPage = () => {
   border-radius: 7px;
   font-size: 32px;
   top: 70%;
+  position: absolute;
 
 `;
   const StyledGreeting = styled.p`
   font-size: 32px;
   top: 40%;
+  position: absolute;
 `;
   const StyledContainer = styled.div`
   display: flex;
@@ -24,47 +27,14 @@ const LandingPage = () => {
 
   return (
     <div>
-      <Navbar>
-        <Navbar.Brand>
-          <Navbar.Item renderAs="a" href="#">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              alt="Bulma: a modern CSS framework based on Flexbox"
-              width="112"
-              height="28"
-            />
-          </Navbar.Item>
-          <Navbar.Burger />
-        </Navbar.Brand>
-        <Navbar.Menu>
-          <Navbar.Container>
-            <Navbar.Item href="#">
-              <Navbar.Link >First</Navbar.Link>
-              <Navbar.Dropdown>
-                <Navbar.Item href="#">Subitem 1</Navbar.Item>
-                <Navbar.Item href="#">Subitem 2</Navbar.Item>
-                <Navbar.Divider />
-                <Navbar.Item href="#">After divider</Navbar.Item>
-              </Navbar.Dropdown>
-            </Navbar.Item>
-            <Navbar.Item href="#">Second</Navbar.Item>
-          </Navbar.Container>
-          <Navbar.Container align="end">
-            <Navbar.Item href="#">At the end</Navbar.Item>
-          </Navbar.Container>
-        </Navbar.Menu>
-      </Navbar>
+      <OrangeNavbar needBackArrow={true}/>
       <StyledContainer>
-        <div>
           <StyledGreeting>
             Welcome, Bob Smith!
           </StyledGreeting>
-        </div>
-        <div>
           <StyledButton>
             Create Order
           </StyledButton>
-        </div>
       </StyledContainer>
     </div>
   )
