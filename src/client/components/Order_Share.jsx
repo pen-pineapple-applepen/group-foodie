e2612e0709383984ca1afdb8b3d8a5a2fd5b83fb
Scott Hansen <scottiehansen@gmail.com>
2021-07-13 15:09:42 -0700

    Added date/time picker css to index.css file. More Notes below: Date/time picker is currently not functional. Need to play with it to restrict times. Added a colored line.

--- src/client/components/Order_Share.jsx
+++ src/client/components/Order_Share.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../state/hooks';
 import { Button } from 'react-bulma-components';
 import DatePicker from "react-datepicker";
+import format from 'date-fns/format';
 
 function OrderShare() {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const ColoredLine = () => (
+    <hr
+        style={{
+            width: '20%',
+            color: '#FF6C36',
+            backgroundColor: '#FF6C36',
+        }}
+    />
+  );
 
   return (
-    <div className='time_and_date_input'>
-      <DatePicker />
+    <div className='order_share'>
+      <div>
+        Enter Date/Time:
+        <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        includeDates={[new Date(), new Date() ]}
+        placeholderText="This only includes today and tomorrow"
+        />
+        <ColoredLine />
+      </div>
+      <div>
+        Copy Link:
+      </div>
     </div>
   )
 
