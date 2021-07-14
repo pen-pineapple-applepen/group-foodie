7af8d1689635646931bb3b2ee2ebcd2ff6351610
Austin Yeon <austinyeon@gmail.com>
2021-07-14 09:19:18 -0700

    working on creating login reducer to toggle state of loggedin

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react';
 import { useAppDispatch, useAppSelector } from '../state/hooks';
 import allActions from '../state/actions/allActions';
-<<<<<<< HEAD
 import styled from 'styled-components';
 import RouterContainer from './RouterContainer';
-=======
-import MainLoginPage from './loginSignUpPage/mainLoginPage';
-import SignUpPage from './loginSignUpPage/SignUpPage';
-import styled from 'styled-components';
->>>>>>> added background image to the signup and login page, finished centering and added more styling
 
 const MobileDiv = styled.div`
 
@@ -19,18 +13,7 @@ function App() {
 // these are just some test examples
   return (
     <div className="App">
-<<<<<<< HEAD
       <RouterContainer />
-=======
-      {/* <p>
-        my name is
-        {name || ''}
-      </p>
-      <Button color="primary" onClick={handleClick}>add Erik</Button> */}
-      <OrangeNavbar needBackArrow={true}/>
-      {/* <MainLoginPage /> */}
-      <SignUpPage />
->>>>>>> added background image to the signup and login page, finished centering and added more styling
     </div>
   );
 }
