12e0951c96b7f9f21c08f307b90b1217486713d0
Austin Yeon <austinyeon@gmail.com>
2021-07-13 23:07:03 -0700

    added background image to the signup and login page, finished centering and added more styling

--- src/client/components/loginSignUpPage/SignUpPage.jsx
+++ src/client/components/loginSignUpPage/SignUpPage.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import { Button, Icon, Form } from 'react-bulma-components';
+import styled from 'styled-components';
+import { OrangeButton } from '/src/client/styles/shared.tsx';
 
 export default function SignUpPage() {
 
+  const StyledHeader = styled.h2`
+  font-size: 20px;
+`;
+
   return (
-    <div className="login-signup-page-container">
-      <h2>New Account</h2>
-      <Form.Field>
-        <Form.Label>Email</Form.Label>
-        <Form.Control>
-          <Form.Input type="text" placeholder="Email" />
-          <Icon align="left" size="small">
-            <i className="fas fa-envelope" />
-          </Icon>
-        </Form.Control>
-        <Form.Label>Password</Form.Label>
-        <Form.Control>
-          <Form.Input type="text" placeholder="Password" />
-          <Icon align="left" size="medium">
-            <i className="fas fa-key" />
-          </Icon>
-        </Form.Control>
-      </Form.Field>
-      <div className='login-buttons'>
-        <Button>Sign Up</Button>
+    <div className="login-signup-background">
+      <div className="login-signup-page-container">
+        <StyledHeader>New Account</StyledHeader>
+        <Form.Field className="login-form">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control>
+            <Form.Input placeholder="First Name" />
+          </Form.Control>
+          <Form.Label>Email</Form.Label>
+          <Form.Control>
+            <Form.Input type="text" placeholder="Email" />
+            <Icon align="left" size="small">
+              <i className="fas fa-envelope" />
+            </Icon>
+          </Form.Control>
+          <Form.Label>Email</Form.Label>
+          <Form.Control>
+            <Form.Input type="text" placeholder="Email" />
+            <Icon align="left" size="small">
+              <i className="fas fa-envelope" />
+            </Icon>
+          </Form.Control>
+          <Form.Label>Password</Form.Label>
+          <Form.Control>
+            <Form.Input type="text" placeholder="Password" />
+            <Icon align="left" size="medium">
+              <i className="fas fa-key" />
+            </Icon>
+          </Form.Control>
+        </Form.Field>
+        <div className='login-buttons'>
+          <OrangeButton>Sign Up</OrangeButton>
+        </div>
       </div>
     </div>
   )
