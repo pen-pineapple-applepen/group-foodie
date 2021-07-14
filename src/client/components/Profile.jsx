7dc5bcae75a0b99c013f4ffdb0791f96cc42c88b
Chris Wu <cwu0569@gmail.com>
2021-07-13 20:11:32 -0700

    used shared components and grabbed currentUser state from redux store

--- src/client/components/Profile.jsx
+++ src/client/components/Profile.jsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from 'react-bulma-components';
+import { useAppDispatch, useAppSelector } from '../state/hooks';
+import { BackArrow, ProfileImage, OrangeButton, OrangeNavbar } from '../styles/shared';
 
 const ProfileDiv = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+const nameText = styled.span`
+
 `;
 
 const Profile = (props) => {
   // create state for user in redux store
   // user has a name?, email, password, username, guest status
+  // user has
+    // firstName
+    // lastName
+    // email
+    // username
+    // guest status
+    // friends array
+    // orders array
+
+  // order objects in the array should have
+    // food name
+    // quantity
+    // price
+    // date
+  const currentUser = useAppSelector((state) => state.currentUser);
 
   return (
     <ProfileDiv>
-      <Button>View Orders</Button>
-      <Button>Friends</Button>
+      <OrangeNavbar needBackArrow={true}/>
+      <nameText>firstName lastName</nameText>
+      <OrangeButton>View Orders</OrangeButton>
+      <OrangeButton>Friends</OrangeButton>
     </ProfileDiv>
   );
 };
