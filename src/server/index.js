22bd776685a86de47ef0724793c12a91b5f4c993
Austin Yeon <austinyeon@gmail.com>
2021-07-16 11:39:07 -0700

    removed the extra app.history in the index

--- src/server/index.js
+++ src/server/index.js
@@ -19,9 +19,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use('/api', router);
 
-
-app.use(history());
-
 app.listen(port, (err) => {
   if (err) {
     console.log('error listening on port: ', err);
