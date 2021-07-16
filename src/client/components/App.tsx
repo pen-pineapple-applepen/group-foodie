1a6aa50ccfcaf7757169889204f4d8e12f252d90
Erik Oh <eoh555@gmail.com>
2021-07-16 12:45:21 -0700

    minor changes with routing

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -20,7 +20,7 @@ import MenuItemPage from './MenuItemPage';
 import RestaurantPage from './RestaurantsPage';
 import ChatPage from './ChatPage';
 import NewPaymentPage from './NewPaymentPage';
-
+import Confirmation from './Confirmation/Confirmation';
 import FriendMenuPage from './friendsView/FriendMenuPage';
 import OrderShare from './OrderShare.jsx';
 
@@ -45,6 +45,8 @@ function App() {
             <Route exact path="/ChatPage" component={ChatPage} />
             <Route exact path="/Menu/Friends/" component={FriendMenuPage} />"
             <Route exact path="/ShareOrder" component={OrderShare} />"
+            <Route exact path="/Confirmation" component={Confirmation} />"
+
             <Redirect to="/" />
           </Switch>
           {/* <NewPaymentPage /> */}
