88a827ff599fc6a2ec7feb217abfb2fa4abea02e
Chris Wu <cwu0569@gmail.com>
2021-07-17 23:03:20 -0700

    changed width of each list to 80%

--- src/client/components/HistoryList.jsx
+++ src/client/components/HistoryList.jsx
@@ -5,7 +5,7 @@ import HistoryListItem from './HistoryListItem';
 const List = styled.ul`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  width: 80%;
 `;
 
 const HistoryList = (props) => {
