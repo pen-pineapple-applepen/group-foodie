fb26a961d2df4afb02d7542ebc29fcd1397b8ec7
Nick Kim <nicholaskim101@yahoo.com>
2021-07-18 18:36:05 -0700

    Added email reducer for email modal

---
+++ src/client/state/reducers/emailFriendsReducer.js
@@ -0,0 +1,25 @@
+import { createReducer, createAction } from '@reduxjs/toolkit';
+
+export const addEmail = createAction('Add_Email');
+export const removeEmail = createAction('Remove_Email');
+export const resetEmails = createAction('Reset_Emails');
+
+let initialState = {
+  emails: [],
+};
+
+export const emailFriendsRecucer = createReducer (initialState, (builder) => {
+  builder
+    .addCase(addEmail, (state, action) => {
+      state.emails.push(action.payload)
+    })
+    .addCase(removeEmail, (state, action) => {
+      let newEmails = state.emails.filter(email => {
+        return email !== action.payload
+      });
+      state.emails = newEmails;
+    })
+    .addCase(resetEmails, (state, action) => {
+      state.emails = [];
+    })
+})
